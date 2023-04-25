c4ab6f2f7a594d5407faf199c24605b16976ff3e
Loic Schillings <12201682@student.pxl.be>
2023-04-25 10:50:41 +0200

    Header TCP

---
+++ UDP/UDP_SERVER.c
@@ -0,0 +1,47 @@
+#ifdef _WIN32
+	#define _WIN32_WINNT _WIN32_WINNT_WIN7
+	#include <winsock2.h> //for all socket programming
+	#include <ws2tcpip.h> //for getaddrinfo, inet_pton, inet_ntop
+	#include <stdio.h> //for fprintf, perror
+	#include <unistd.h> //for close
+	#include <stdlib.h> //for exit
+	#include <string.h> //for memset
+
+  #else
+	#include <sys/socket.h> //for sockaddr, socket, socket
+	#include <sys/types.h> //for size_t
+	#include <netdb.h> //for getaddrinfo
+	#include <netinet/in.h> //for sockaddr_in
+	#include <arpa/inet.h> //for htons, htonl, inet_pton, inet_ntop
+	#include <errno.h> //for errno
+	#include <stdio.h> //for fprintf, perror
+	#include <unistd.h> //for close
+	#include <stdlib.h> //for exit
+	#include <string.h> //for memset
+#endif
+
+int main( int argc, char * argv[] )
+{
+	//////////////////
+	//Initialization//
+	//////////////////
+
+  int internet_socket = initialization();
+
+  /////////////
+	//Execution//
+	/////////////
+
+  ////////////
+	//Clean up//
+	////////////
+
+  cleanup( internet_socket );
+
+  return 0;
+}
+
+void cleanup( int internet_socket )
+{
+	close( internet_socket );
+}
